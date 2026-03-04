a8b7df5663b83875b3857bedceb619a44fa26603
Yingtao Li <3551776093@qq.com>
2026-03-04 21:45:10 +0800

    Add configurable random frame subsampling for RGV49 training

--- src/vggt/layers/vision_transformer.py
+++ src/vggt/layers/vision_transformer.py
@@ -95,6 +95,7 @@ class DinoVisionTransformer(nn.Module):
 
         # tricky but makes it work
         self.use_checkpoint = False
+        self.use_reentrant = False
         #
 
         self.num_features = self.embed_dim = embed_dim  # num_features for consistency with other models
